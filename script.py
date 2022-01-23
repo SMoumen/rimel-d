e604ed0e04f014cea7fa3b9e61ea3d2f68abf4e6
SMoumen <moumensofian@gmail.com>
2022-01-23 12:43:14 +0100

    fix linters errors

--- script.py
+++ script.py
@@ -1,78 +1,77 @@
 from abc import ABC, abstractmethod
 
 
-class Answer :
-    
-    def __init__(self,goodPractice) -> None:
+class Answer:
+    def __init__(self, goodPractice) -> None:
         self.goodPractice = goodPractice
 
-    
-    def printAnswer(self) :
-        print("EVALUATED GOOD PRACTICE : " + self.goodPractice.name + " grade : " + str(self.goodPractice.grade) + "\n criticity : " + self.goodPractice.criticity + self.goodPractice.comment)
-    
-    
-   
+    def printAnswer(self):
+        print(
+            "EVALUATED GOOD PRACTICE : "
+            + self.goodPractice.name
+            + " grade : "
+            + str(self.goodPractice.grade)
+            + "\n criticity : "
+            + self.goodPractice.criticity
+            + self.goodPractice.comment
+        )
 
-class GoodPractice(ABC) :
-    
+
+class GoodPractice(ABC):
+    @abstractmethod
+    def evaluate(self):
+        return 0
 
     @abstractmethod
-    def evaluate(self) : return 0
-    
+    def parse(self):
+        return
+
     @abstractmethod
-    def parse(self) : return 
-    
-    @abstractmethod 
-    def generateComment(self) : return
+    def generateComment(self):
+        return
 
 
-class GPHasName(GoodPractice) :
-    
-    
-    def __init__(self,name,criticity,grade =0 ,maxgrade = 0,comment = "") : 
+class GPHasName(GoodPractice):
+    def __init__(self, name, criticity, grade=0, maxgrade=0, comment=""):
         self.grade = grade
         self.maxgrade = maxgrade
         self.name = name
-        self.criticity =criticity
+        self.criticity = criticity
         self.comment = comment
-    def evaluate(self) : return self.grade/self.maxgrade       
-    
-    def generateComment(self) : 
-        if(self.grade == self.maxgrade) : self.comment = " You have correctly respected the " + self.name + " good practice! "
-        elif(self.grade < self.maxgrade / 2) : self.comment = " Warning ! You should name your rule files in the main.yml file" 
-          
-    def parse(self) : 
-        f = open("main.yml","r")
+
+    def evaluate(self):
+        return self.grade / self.maxgrade
+
+    def generateComment(self):
+        if self.grade == self.maxgrade:
+            self.comment = (
+                " You have correctly respected the " + self.name + " good practice! "
+            )
+        elif self.grade < self.maxgrade / 2:
+            self.comment = (
+                " Warning ! You should name your rule files in the main.yml file"
+            )
+
+    def parse(self):
+        f = open("main.yml", "r")
         str = "name:"
         counter = 0
         gradeCounter = 0
-        for line in f : 
-            if(str not in line) : continue
-            if(line.split("- name:",1)[1] != ""):
-                #print("name isnt empty : " + line.split("- name:",1)[1])
+        for line in f:
+            if str not in line:
+                continue
+            if line.split("- name:", 1)[1] != "":
+                # print("name isnt empty : " + line.split("- name:",1)[1])
                 gradeCounter += 1
             counter += 1
-            
+
         self.maxgrade = counter
         self.grade = gradeCounter
         self.generateComment()
-        
-
-        
-   
-   
 
 
-
-
-c = GPHasName("Naming","Lowest")
+c = GPHasName("Naming", "Lowest")
 c.parse()
 print(c.evaluate())
 a = Answer(c)
 a.printAnswer()
-
-        
-    
-    
-
-
